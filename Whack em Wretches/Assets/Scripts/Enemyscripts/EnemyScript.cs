86bf66cf9ea15c1596e86bce38ac7de21abb294c
LARGERONThibault <t.largeron@bdx.ecolescreatives.com>
2026-02-23 15:28:41 +0100

    ADDED deathanimation to enemies
    Added deathanimation particles to ennemies.

--- Whack em Wretches/Assets/Scripts/Enemyscripts/EnemyScript.cs
+++ Whack em Wretches/Assets/Scripts/Enemyscripts/EnemyScript.cs
@@ -7,9 +7,15 @@ public class EnemyScript : MonoBehaviour
     public Transform playerTransform;
     public float speed;
     public float might;
+    public GameObject deathanimation;
 
     void Awake()
     {
         playerTransform = GameObject.Find("Main Camera").GetComponent<Transform>();
+    }
+    void OnDestroy()
+    {
+        GameObject mort = Instantiate(deathanimation);
+        mort.transform.position = transform.position;
     }
 }
